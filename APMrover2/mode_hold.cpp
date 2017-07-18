4f1eb17df10e06eff5b4adb1328118595935c44a
Randy Mackay <rmackay9@yahoo.com>
2017-07-18 16:28:14 +0900

    Rover: mode hold formatting

--- APMrover2/mode_hold.cpp
+++ APMrover2/mode_hold.cpp
@@ -1,7 +1,8 @@
 #include "mode.h"
 #include "Rover.h"
 
-void ModeHold::update() {
+void ModeHold::update()
+{
     // hold position - stop motors and center steering
     g2.motors.set_throttle(0.0f);
     g2.motors.set_steering(0.0f);
