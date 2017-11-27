f62799284ff38ad893c124a0c6a874af8776f7d8
Randy Mackay <rmackay9@yahoo.com>
2017-11-27 18:16:31 +0900

    AP_MotorsUGV: skid steering always uses full range
    Allows skid friction to be removed

--- APMrover2/AP_MotorsUGV.h
+++ APMrover2/AP_MotorsUGV.h
@@ -96,7 +96,6 @@ protected:
     AP_Int16 _slew_rate; // slew rate expressed as a percentage / second
     AP_Int8 _throttle_min; // throttle minimum percentage
     AP_Int8 _throttle_max; // throttle maximum percentage
-    AP_Float _skid_friction;    // skid steering vehicle motor output compensation for friction while stopped
 
     // internal variables
     float   _steering;  // requested steering as a value from -4500 to +4500
