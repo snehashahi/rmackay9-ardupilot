ArduPilot/ardupilot

Copter: FlightMode - convert FLIP flight mode

ArduCopter/FlightMode.h
@@ -612,3 +612,33 @@ protected:
 private:
 
 };
+
+
+
+class FlightMode_FLIP : public FlightMode {
+
+public:
+
+    FlightMode_FLIP(Copter &copter) :
+        Copter::FlightMode(copter)
+        { }
+
+    virtual bool init(bool ignore_checks) override;
+    virtual void run() override; // should be called at 100hz or more
+
+    virtual bool requires_GPS() const override { return false; }
+    virtual bool has_manual_throttle() const override { return false; }
+    virtual bool allows_arming(bool from_gcs) const override { return false; };
+    virtual bool is_autopilot() const override { return false; }
+
+protected:
+
+    const char *name() const override { return "FLIP"; }
+    const char *name4() const override { return "FLIP"; }
+
+private:
+
+    // Flip
+    Vector3f flip_orig_attitude;         // original copter attitude before flip
+
+};

snehashahi/rmackay9-ardupilot

ArduCopter/FlightMode.h
@@ -633,3 +633,33 @@ protected:
 private:
 
 };
+
+
+
+class FlightMode_FLIP : public FlightMode {
+
+public:
+
+    FlightMode_FLIP(Copter &copter) :
+        Copter::FlightMode(copter)
+        { }
+
+    virtual bool init(bool ignore_checks) override;
+    virtual void run() override; // should be called at 100hz or more
+
+    virtual bool requires_GPS() const override { return false; }
+    virtual bool has_manual_throttle() const override { return false; }
+    virtual bool allows_arming(bool from_gcs) const override { return false; };
+    virtual bool is_autopilot() const override { return false; }
+
+protected:
+
+    const char *name() const override { return "FLIP"; }
+    const char *name4() const override { return "FLIP"; }
+
+private:
+
+    // Flip
+    Vector3f flip_orig_attitude;         // original vehicle attitude before flip
+
+};