71adbb7a7aea4a1b52eb2a5fe7da3cb5fde12b95
Leonard Hall <leonardthall@gmail.com>
2018-01-06 17:16:10 +1030

    AC_AttitudeControl: Add EKF Reset functionality and some changes for clarity

--- libraries/AC_AttitudeControl/AC_AttitudeControl.h
+++ libraries/AC_AttitudeControl/AC_AttitudeControl.h
@@ -125,6 +125,9 @@ public:
     // Shifts earth frame yaw target by yaw_shift_cd. yaw_shift_cd should be in centidegrees and is added to the current target heading
     void shift_ef_yaw_target(float yaw_shift_cd);
 
+    // Shifts earth frame yaw target by yaw_shift_cd. yaw_shift_cd should be in centidegrees and is added to the current target heading
+    void inertial_frame_reset();
+
     // Command a Quaternion attitude with feedforward and smoothing
     void input_quaternion(Quaternion attitude_desired_quat);
 
@@ -378,12 +381,11 @@ protected:
     // the target attitude frame.
     Vector3f            _attitude_target_ang_vel;
 
-    // This represents the angular velocity in radians per second, used in the angular
+    // This represents the angular velocity in radians per second in the body frame, used in the angular
     // velocity controller.
     Vector3f            _rate_target_ang_vel;
 
-    // This represents a quaternion attitude error, used in the angular
-    // velocity controller and for monitoring purposes.
+    // This represents a quaternion attitude error in the body frame, used for inertial frame reset handling.
     Quaternion          _attitude_ang_error;
 
     // The angle between the target thrust vector and the current thrust vector.
