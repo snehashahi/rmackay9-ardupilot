ac33b4a700c0b770b408d65ed22e3e9ead59cb47
Randy Mackay <rmackay9@yahoo.com>
2018-01-25 21:35:05 +0900

    AP_Follow: library to track and follow another vehicle

---
+++ libraries/AP_Follow/AP_Follow.cpp
@@ -0,0 +1,304 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <AP_HAL/AP_HAL.h>
+#include <GCS_MAVLink/GCS.h>
+#include "AP_Follow.h"
+#include <ctype.h>
+#include <stdio.h>
+
+extern const AP_HAL::HAL& hal;
+
+#define AP_FOLLOW_TIMEOUT_MS    1000    // position estimate timeout after 1 second
+
+#define AP_FOLLOW_OFFSET_TYPE_NED       0   // offsets are in north-east-down frame
+#define AP_FOLLOW_OFFSET_TYPE_RELATIVE  0   // offsets are relative to lead vehicle's heading
+
+// table of user settable parameters
+const AP_Param::GroupInfo AP_Follow::var_info[] = {
+
+    // @Param: _ENABLE
+    // @DisplayName: Follow enable/disable
+    // @Description: Enabled/disable following a target
+    // @Values: 0:Disabled,1:Enabled
+    // @User: Standard
+    AP_GROUPINFO_FLAGS("_ENABLE", 1, AP_Follow, _enabled, 0, AP_PARAM_FLAG_ENABLE),
+
+    // @Param: _TYPE
+    // @DisplayName: Follow type
+    // @Description: Follow type
+    // @Values: 0:Keep Initial Offset, 1:Maintain Margin
+    // @User: Standard
+    AP_GROUPINFO("_TYPE", 2, AP_Follow, _type, 0),
+
+    // @Param: _SYSID
+    // @DisplayName: Follow target's mavlink system id
+    // @Description: Follow target's mavlink system id
+    // @Range: 0 255
+    // @User: Standard
+    AP_GROUPINFO("_SYSID", 3, AP_Follow, _target_sysid, 2),
+
+    // @Param: _MARGIN
+    // @DisplayName: Follow minimum distance margin
+    // @Description: Follow minimum distance margin
+    // @Units: m
+    // @Range: 1 10
+    // @User: Standard
+    AP_GROUPINFO("_MARGIN", 4, AP_Follow, _margin, 5),
+
+    // @Param: _DIST_MAX
+    // @DisplayName: Follow distance maximum
+    // @Description: Follow distance maximum.  targets further than this will be ignored
+    // @Units: m
+    // @Range: 1 1000
+    // @User: Standard
+    AP_GROUPINFO("_DIST_MAX", 5, AP_Follow, _dist_max, 100),
+
+    // @Param: _OFS_TYPE
+    // @DisplayName: Follow offset type
+    // @Description: Follow offset type
+    // @Values: 0:North-East-Down, 1:Relative to lead vehicle heading
+    // @User: Standard
+    AP_GROUPINFO("_OFS_TYPE", 6, AP_Follow, _offset_type, AP_FOLLOW_OFFSET_TYPE_NED),
+
+    // @Param: _OFS_X
+    // @DisplayName: Follow offsets in meters north/forward
+    // @Description: Follow offsets in meters north/forward.  If positive, this vehicle fly ahead or north of lead vehicle
+    // @Range: -100 100
+    // @Units: m
+    // @Increment: 1
+    // @User: Standard
+
+    // @Param: _OFS_Y
+    // @DisplayName: Follow offsets in meters east/right
+    // @Description: Follow offsets in meters east/right.  If positive, this vehicle fly to the right or east of lead vehicle
+    // @Range: -100 100
+    // @Units: m
+    // @Increment: 1
+    // @User: Standard
+
+    // @Param: _OFS_Z
+    // @DisplayName: Follow offsets in meters down
+    // @Description: Follow offsets in meters down.  If positive, this vehicle fly below the lead vehicle
+    // @Range: -100 100
+    // @Units: m
+    // @Increment: 1
+    // @User: Standard
+    AP_GROUPINFO("_OFS", 7, AP_Follow, _offset, 0),
+
+    // @Param: _YAW_BEHAVE
+    // @DisplayName: Follow yaw behaviour
+    // @Description: Follow yaw behaviour
+    // @Values: 0:None,1:Face Lead Vehicle,2:Same as Lead vehicle,3:Direction of Flight
+    // @User: Standard
+    AP_GROUPINFO("_YAW_BEHAVE", 8, AP_Follow, _yaw_behave, 1),
+
+    AP_GROUPEND
+};
+
+/* 
+   The constructor also initialises the proximity sensor. Note that this
+   constructor is not called until detect() returns true, so we
+   already know that we should setup the proximity sensor
+*/
+AP_Follow::AP_Follow(const AP_AHRS &ahrs) :
+        _ahrs(ahrs)
+{
+    AP_Param::setup_object_defaults(this, var_info);
+}
+
+// initialise follow subsystem
+void AP_Follow::init()
+{
+    // nothing to do
+}
+
+// update the state of the sensor
+void AP_Follow::update(void)
+{
+    // exit immediately if not enabled
+    if (!_enabled) {
+        _healthy = false;
+    }
+
+    // check for timeout and set health status
+    if (_last_location_update_ms == 0 || (AP_HAL::millis() - _last_location_update_ms > AP_FOLLOW_TIMEOUT_MS)) {
+        _healthy = false;
+    } else {
+        _healthy = true;
+    }
+}
+
+// true if we have a valid target location estimate
+bool AP_Follow::have_target() const
+{
+    // exit immediately if not enabled
+    if (!_enabled) {
+        return false;
+    }
+
+    // check for timeout
+    if ((_last_location_update_ms == 0) || (AP_HAL::millis() - _last_location_update_ms > AP_FOLLOW_TIMEOUT_MS)) {
+        return false;
+    }
+
+    // we must have a valid target estimate
+    return true;
+}
+
+// get target's estimated location
+bool AP_Follow::get_target_location_and_velocity(Location &loc, Vector3f& vel) const
+{
+    // exit immediately if not enabled
+    if (!_enabled) {
+        return false;
+    }
+
+    // check for timeout
+    if ((_last_location_update_ms == 0) || (AP_HAL::millis() - _last_location_update_ms > AP_FOLLOW_TIMEOUT_MS)) {
+        return false;
+    }
+
+    // calculate time since last actual position update
+    float dt = (AP_HAL::millis() - _last_location_update_ms) / 1000.0f;
+
+    // get velocity estimate
+    if (!get_velocity_ned(vel, dt)) {
+        return false;
+    }
+
+    // project the vehicle position
+    Location last_loc = _target_location;
+    location_offset(last_loc, vel.x * dt, vel.y * dt);
+    last_loc.alt -= vel.z * 10.0f * dt; // convert m/s to cm/s, multiply by dt.  minus because NED
+
+    // return latest position estimate
+    loc = last_loc;
+    return true;
+}
+
+// get distance vector to target (in meters) and target's velocity all in NED frame
+bool AP_Follow::get_target_dist_and_vel_ned(Vector3f &dist, Vector3f &dist_with_offs, Vector3f &vel) const
+{
+    // get our location
+    Location current_loc;
+    if (!_ahrs.get_position(current_loc)) {
+         return false;
+     }
+
+    // get offsets
+    Vector3f offsets;
+    if (!get_offsets_ned(offsets)) {
+        return false;
+    }
+
+    // get target location and velocity
+    Location target_loc;
+    if (!get_target_location_and_velocity(target_loc, vel)) {
+        return false;
+    }
+
+    // calculate difference
+    Vector3f dist_vec = location_3d_diff_NED(current_loc, target_loc);
+
+    // return results
+    dist = dist_vec;
+    dist_with_offs = dist_vec + offsets;
+    return true;
+}
+
+// get target's heading in degrees (0 = north, 90 = east)
+bool AP_Follow::get_target_heading(float &heading) const
+{
+    // exit immediately if not enabled
+    if (!_enabled) {
+        return false;
+    }
+
+    // check for timeout
+    if ((_last_heading_update_ms == 0) || (AP_HAL::millis() - _last_heading_update_ms > AP_FOLLOW_TIMEOUT_MS)) {
+        return false;
+    }
+
+    // return latest heading estimate
+    heading = _target_heading;
+    return true;
+}
+
+// handle mavlink DISTANCE_SENSOR messages
+void AP_Follow::handle_msg(const mavlink_message_t &msg)
+{
+    // exit immediately if not enabled
+    if (!_enabled) {
+        return;
+    }
+
+    // skip message if not from our target
+    if (msg.sysid != _target_sysid) {
+        return;
+    }
+
+    // decode global-position-int message
+    if (msg.msgid == MAVLINK_MSG_ID_GLOBAL_POSITION_INT) {
+        uint32_t now = AP_HAL::millis();
+        // decode message
+        mavlink_global_position_int_t packet;
+        mavlink_msg_global_position_int_decode(&msg, &packet);
+        _target_location.lat = packet.lat;
+        _target_location.lng = packet.lon;
+        _target_location.alt = packet.alt/10;       // convert millimeters to cm
+        _target_velocity_ned.x = packet.vx/100.0f;  // velocity north
+        _target_velocity_ned.y = packet.vy/100.0f;  // velocity east
+        _target_velocity_ned.z = packet.vz/100.0f;  // velocity down
+        _last_location_update_ms = now;
+        if (packet.hdg <= 36000) {                  // heading (UINT16_MAX if unknown)
+            _target_heading = packet.hdg / 100.0f;  // convert centi-degrees to degrees
+            _last_heading_update_ms = now;
+        }
+    }
+}
+
+// get velocity estimate in NED frame
+bool AP_Follow::get_velocity_ned(Vector3f& vel, float dt) const
+{
+    vel = _target_velocity_ned + (_target_accel_ned * dt);
+    return true;
+}
+
+// get offsets in NED frame
+bool AP_Follow::get_offsets_ned(Vector3f& offset) const
+{
+    const Vector3f &off = _offset.get();
+
+    // if offsets are zero or type if NED, simply return offset vector
+    if (off.is_zero() || (_offset_type == AP_FOLLOW_OFFSET_TYPE_NED)) {
+        offset = off;
+        return true;
+    }
+
+    // offset_type == AP_FOLLOW_OFFSET_TYPE_RELATIVE
+    // check if we have a valid heading for target vehicle
+    if ((_last_heading_update_ms == 0) || (AP_HAL::millis() - _last_heading_update_ms > AP_FOLLOW_TIMEOUT_MS)) {
+        return false;
+    }
+
+    // rotate roll, pitch input from north facing to vehicle's perspective
+    const float veh_cos_yaw = cosf(radians(_target_heading));
+    const float veh_sin_yaw = sinf(radians(_target_heading));
+    offset.x = (off.x * veh_cos_yaw) - (off.y * veh_sin_yaw);
+    offset.y = (off.y * veh_cos_yaw) + (off.x * veh_sin_yaw);
+    offset.z = off.z;
+    return true;
+}
---
+++ libraries/AP_Follow/AP_Follow.h
@@ -0,0 +1,107 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include <AP_Common/AP_Common.h>
+#include <AP_HAL/AP_HAL.h>
+#include <AP_Param/AP_Param.h>
+#include <AP_Math/AP_Math.h>
+#include <AP_AHRS/AP_AHRS.h>
+#include <GCS_MAVLink/GCS.h>
+
+class AP_Follow
+{
+
+public:
+
+    // enum for YAW_BEHAVE parameter
+    enum YawBehave {
+        YAW_BEHAVE_NONE = 0,
+        YAW_BEHAVE_FACE_LEAD_VEHICLE = 1,
+        YAW_BEHAVE_SAME_AS_LEAD_VEHICLE = 2,
+        YAW_BEHAVE_DIR_OF_FLIGHT = 3
+    };
+
+    // constructor
+    AP_Follow(const AP_AHRS &ahrs);
+
+    // initialise follow subsystem
+    void init();
+
+    // update follow subsystem
+    void update();
+
+    // true if follow subsystem is healthy (i.e. we are receiving messages)
+    bool healthy() const { return _healthy; }
+
+    //
+    // position tracking related methods
+    //
+
+    // true if we have a valid target location estimate
+    bool have_target() const;
+
+    // get target's estimated location and velocity (in NED)
+    bool get_target_location_and_velocity(Location &loc, Vector3f &vel) const;
+
+    // get distance vector to target (in meters), target plus offsets, and target's velocity all in NED frame
+    bool get_target_dist_and_vel_ned(Vector3f &dist, Vector3f &dist_with_ofs, Vector3f &vel) const;
+
+    //
+    // yaw/heading related methods
+    //
+
+    // get user defined yaw behaviour
+    YawBehave get_yaw_behave() const { return (YawBehave)_yaw_behave.get(); }
+
+    // get target's heading in degrees (0 = north, 90 = east)
+    bool get_target_heading(float &heading) const;
+
+    // parse mavlink messages which may hold target's position, velocity and attitude
+    void handle_msg(const mavlink_message_t &msg);
+
+    // parameter list
+    static const struct AP_Param::GroupInfo var_info[];
+
+private:
+
+    // get velocity estimate in NED frame using dt since last update
+    bool get_velocity_ned(Vector3f& vel, float dt) const;
+
+    // get offsets in NED frame
+    bool get_offsets_ned(Vector3f& offsets) const;
+
+    // references
+    const AP_AHRS &_ahrs;
+
+    // parameters
+    AP_Int8     _enabled;           // 1 if this subsystem is enabled
+    AP_Int8     _type;              // follow type (keep initial offset, maintain margin)
+    AP_Int16    _target_sysid;      // target's mavlink system id
+    AP_Float    _margin;            // minimum target distance to target
+    AP_Float    _dist_max;          // maximum distance to target.  targets further than this will be ignored
+    AP_Int8     _offset_type;       // offset frame type (0:North-East-Down, 1:RelativeToLeadVehicleHeading)
+    AP_Vector3f _offset;            // offset from lead vehicle in meters
+    AP_Int8     _yaw_behave;        // following vehicle's yaw/heading behaviour
+
+    // local variables
+    bool _healthy;                  // true if we are receiving mavlink messages (regardless of whether they have target position info within them)
+    uint32_t _last_location_update_ms;  // system time of last position update
+    Location _target_location;      // last known location of target
+    Vector3f _target_velocity_ned;  // last known velocity of target in NED frame in m/s
+    Vector3f _target_accel_ned;     // last known acceleration of target in NED frame in m/s/s
+    uint32_t _last_heading_update_ms;   // system time of last heading update
+    float _target_heading;          // heading in degrees
+};
