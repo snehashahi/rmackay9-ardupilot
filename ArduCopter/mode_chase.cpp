337e7af789ba7f22b623cf4e2d651276803ee62a
Randy Mackay <rmackay9@yahoo.com>
2018-01-29 21:38:04 +0900

    Copter: remove lonely mode from chase

--- ArduCopter/mode_chase.cpp
+++ ArduCopter/mode_chase.cpp
@@ -9,6 +9,7 @@
  * TODO: "channel 7 option" to lock onto "pointed at" target
  * TODO: do better in terms of loitering around the moving point; may need a PID?  Maybe use loiter controller somehow?
  * TODO: extrapolate target vehicle position using its velocity and acceleration
+ * TODO: ensure AC_AVOID_ENABLED is true because we rely on it velocity limiting functions
  */
 
 #if 1
@@ -34,25 +35,6 @@ bool Copter::ModeChase::set_velocity(const Vector3f& velocity_neu)
     return true;
 }
 
-void Copter::ModeChase::run_lonely_mode()
-{
-    if (lonely_mode == nullptr) {
-        if (copter.mode_loiter.init(false)) {
-            lonely_mode = &copter.mode_loiter;
-        } else if(copter.mode_rtl.init(false)) {
-            lonely_mode = &copter.mode_rtl;
-        } else {
-            copter.mode_land.init(false);
-            lonely_mode = &copter.mode_land;
-        }
-
-        gcs().send_text(MAV_SEVERITY_INFO, "Chase: Lonely; %s", lonely_mode->name());
-    }
-
-    lonely_mode->run();
-}
-
-
 void Copter::ModeChase::run()
 {
     // if not auto armed or motor interlock not enabled set throttle to zero and exit immediately
@@ -65,70 +47,42 @@ void Copter::ModeChase::run()
     // Note: this is safe from interference from GCSs and companion computer's whose guided mode
     //       position and velocity requests will be ignored while the vehicle is not in guided mode
 
-    const uint32_t now = AP_HAL::millis();
-    if (now - target_last_update_ms > target_update_timeout_ms) {
-        return run_lonely_mode();
+    Vector3f desired_velocity;
+
+    Vector3f dist_vec_to_target;
+    if (g2.follow.get_distance_to_target_ned(dist_vec_to_target)) {
+        // debug
+        Debug("dist to vec: %f %f %f", (double)dist_vec_to_target.x, (double)dist_vec_to_target.y, (double)dist_vec_to_target.z);
+
+        // set direction vector towards target
+        Vector3f dir_to_target = dist_vec_to_target;
+        dir_to_target.normalize();
+
+        // set desired velocity to maximum speed towards target
+        desired_velocity = dir_to_target.normalized() * closure_speed;
+
+        // calculate 2d unit vector towards target
+        const Vector2f dir_to_target_xy = Vector2f(desired_velocity.x, desired_velocity.y).normalized();
+        Vector2f desired_velocity_xy = Vector2f(desired_velocity.x, desired_velocity.y);
+        const float dist_to_target_xy = safe_sqrt(sq(dist_vec_to_target.x)+sq(dist_vec_to_target.y));
+
+        // slow down as we approach target (horizontal axis)
+        _copter.avoid.limit_velocity(pos_control->get_pos_xy_p().kP().get(), pos_control->get_accel_xy(), desired_velocity_xy, dir_to_target_xy, dist_to_target_xy, _copter.G_Dt);
+
+        // limit the horizontal velocity to prevent fence violations
+        _copter.avoid.adjust_velocity(pos_control->get_pos_xy_p().kP().get(), pos_control->get_accel_xy(), desired_velocity_xy, G_Dt);
+
+        // copy horizontal velocity limits back to 3d vector
+        desired_velocity.x = desired_velocity_xy.x;
+        desired_velocity.y = desired_velocity_xy.y;
+
+        // get avoidance adjusted climb rate
+        desired_velocity.z = get_avoidance_adjusted_climbrate(desired_velocity.z);
     }
-
-    Vector3f to_vehicle = location_3d_diff_NED(_copter.current_loc, target_loc);
-    Debug("to_vehicle: %f %f %f", to_vehicle.x, to_vehicle.y, to_vehicle.z);
-    const float distance_to_vehicle = to_vehicle.length();
-
-    if (distance_to_vehicle > sphere_radius_max) {
-        return run_lonely_mode();
-    }
-    lonely_mode = nullptr;
-
-    const float distance_to_stop = pos_control->get_stopping_distance_xyz() * 0.01f;
-
-    const float distance_to_move = distance_to_vehicle - sphere_radius_min;
-    Debug("distance_to_vehicle=%f move=%f stop=%f",
-          distance_to_vehicle,
-          distance_to_move,
-          distance_to_stop);
-    to_vehicle.normalize();
-    if (fabsf(distance_to_move) > distance_to_stop) {
-        to_vehicle *= closure_speed * 100; // m/s to cm/s (which set_velocity takes)
-        to_vehicle.z = -to_vehicle.z; // translate to NEU
-        if (distance_to_move < 0) {
-            to_vehicle = -to_vehicle; // too close!  back up!
-        }
-    } else {
-        to_vehicle.x = 0;
-        to_vehicle.y = 0;
-        to_vehicle.z = 0;
-    }
-    to_vehicle += target_vel;
 
     // re-use guided mode's velocity controller (takes NEU)
-    Copter::ModeGuided::set_velocity(to_vehicle);
+    Copter::ModeGuided::set_velocity(desired_velocity);
 
     Copter::ModeGuided::run();
 }
 
-
-
-void Copter::ModeChase::mavlink_packet_received(const mavlink_message_t &msg)
-{
-    if (copter.flightmode != &copter.mode_chase) {
-        return;
-    }
-    if (msg.sysid != target_srcid) {
-        return;
-    }
-    if (msg.msgid != MAVLINK_MSG_ID_GLOBAL_POSITION_INT) {
-        // handle position only for now
-        return;
-    }
-
-    mavlink_global_position_int_t packet;
-    mavlink_msg_global_position_int_decode(&msg, &packet);
-    target_loc.lat = packet.lat;
-    target_loc.lng = packet.lon;
-    target_loc.alt = packet.relative_alt / 10; // mm -> cm
-    target_vel.x = packet.vx/100.0f; // cm/s to m/s
-    target_vel.y = packet.vy/100.0f; // cm/s to m/s
-    target_vel.z = packet.vz/100.0f; // cm/s to m/s
-
-    target_last_update_ms = AP_HAL::millis();
-}
