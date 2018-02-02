0dceb1d52dab25a8661e784a78416ac115ce24b6
Randy Mackay <rmackay9@yahoo.com>
2018-02-02 20:27:53 +0900

    AC_PosControl: minor comment fix to calc_leash_length_z
    can probably be merged with "AC_PosControl: combine z position control into single method "

--- libraries/AC_AttitudeControl/AC_PosControl.h
+++ libraries/AC_AttitudeControl/AC_PosControl.h
@@ -87,7 +87,7 @@ public:
     float get_accel_z() const { return _accel_z_cms; }
 
     /// calc_leash_length - calculates the vertical leash lengths from maximum speed, acceleration
-    ///     called by pos_to_rate_z if z-axis speed or accelerations are changed
+    ///     called by update_z_controller if z-axis speed or accelerations are changed
     void calc_leash_length_z();
 
     /// set_alt_target - set altitude target in cm above home
