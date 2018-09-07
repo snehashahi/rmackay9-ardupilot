e2f2c57aa1dc3b239ee9115438e03feed553ce44
IamPete1 <33176108+IamPete1@users.noreply.github.com>
2018-09-07 12:07:23 +0900

    RC_Channel: add Sailboat Tack to aux switch

--- libraries/RC_Channel/RC_Channel.h
+++ libraries/RC_Channel/RC_Channel.h
@@ -169,6 +169,7 @@ public:
         SIMPLE       =        59, // simple mode
         ZIGZAG       =        60, // zigzag mode
         ZIGZAG_SaveWP =       61, // zigzag save waypoint
+        SAILBOAT_TACK =       62, // rover sailboat tack
         // if you add something here, make sure to update the documentation of the parameter in RC_Channel.cpp!
         // also, if you add an option >255, you will need to fix duplicate_options_exist
     };
